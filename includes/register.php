eb02ab7dfdbb4527f51c71b37ec51a0da2b12908
Roberto <mgroberto38@hotmail.com>
2021-05-06 18:35:09 -0500

    este dato es de la mejor forma

---
+++ includes/register.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href = "/css/style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div>    
+                    <a href="" > Inicio</a>
+                    <a href="">Quien somos?</a>
+                    <a href=""> Templates</a>
+                    <a href="/web/login.html">Login</a>
+                    <a href="">Registro</a>
+                </div>
+            </nav>
+        </header>
+        <section>
+            <?php
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+                $connfirmation  = $_POST["confirmation"];
+                if ($password == $connfirmation)
+                {
+                    echo "<a href = '/index.php'>volver</a>";
+                }
+                else
+                {
+                    echo "Su contraseña es incorrecta";
+                }
+            ?>          
+        </section>
+        <footer>
+
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href = "/css/style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div>
+                    <div class = "login">
+                        <a href="/web/login.html">Login</a>
+                        <a href="/web/register.html">Registro</a><br>
+                    </div>
+                    <a href="/index.php" >Inicio</a>
+                    <a href="">Quien somos?</a>
+                    <a href="">Templates</a>
+                
+                </div>
+            </nav>
+        </header>
+        <section>
+           
+        </section>
+        <footer>
+
+        </footer>
+    </body>
+</html>
